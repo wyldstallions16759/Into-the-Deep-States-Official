b03b7a180c3dfd9275ddd4cd07a6d406e172934b
3koo2 <egravedp@gmail.com>
2025-02-16 16:07:42 -0500

    Editited TestServoArmOpMode.java to use both servomotors but have an adjustable (set at init time) min/max range to tune servos.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestServoArmOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestServoArmOpMode.java
@@ -8,15 +8,33 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Test Arm Teleop")
 public class TestServoArmOpMode extends LinearOpMode {
+    private double distance = 0;
+
     //declare members
     private Servo armServo1;
     private Servo armServo2;
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
         armServo1 = hardwareMap.get(Servo.class, "leftArmServo");
+        armServo2 = hardwareMap.get(Servo.class, "rightArmServo");
+
+        while (opModeInInit()){
+            if (gamepad1.dpad_up){
+                distance+=0.01;
+            }
+            else if(gamepad1.dpad_down){
+                distance -= 0.01;
+            }
+            telemetry.addData("D=",distance);
+            telemetry.update();
+            Thread.sleep(100);
+        }
 
         waitForStart();
+
+        armServo1.scaleRange(0, distance);
+        armServo2.scaleRange(1-distance, 1);
 
         while (opModeIsActive()){
             boolean up = gamepad1.dpad_up;
@@ -24,11 +42,11 @@ public class TestServoArmOpMode extends LinearOpMode {
 
             if (up){
                 armServo1.setPosition(0);
-                //armServo2.setPosition(1);
+                armServo2.setPosition(1);
             }
             else if (down){
                 armServo1.setPosition(1);
-                //armServo2.setPosition(0);
+                armServo2.setPosition(0);
             }
         }
     }
