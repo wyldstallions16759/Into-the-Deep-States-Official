14087416e30009a17b7e36f1067a8caa2310a95e
Patrick DeBickes <psdebickes@gmail.com>
2025-02-19 17:55:03 -0500

    very unfinished distance calculator with limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightSubsystemTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightSubsystemTest.java
@@ -10,9 +10,13 @@ import org.firstinspires.ftc.teamcode.subsystems.LimelightSubsystem;
 
 import java.util.List;
 
-@Autonomous(name="LimelightTest")
+@TeleOp(name="LimelightTest")
 public class LimelightSubsystemTest extends LinearOpMode{
     private Limelight3A limelight;
+    static final double camHeightOffGround = 13;
+    double blockHeightOffGround = 0;
+    double heightOffGround = blockHeightOffGround-camHeightOffGround;
+    static final double a1 = 0;
 
     public void runOpMode(){
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
@@ -22,15 +26,19 @@ public class LimelightSubsystemTest extends LinearOpMode{
         waitForStart();
 
         while (opModeIsActive()){
-
+            boolean changer = gamepad2.y;
+            double heightOffGround = blockHeightOffGround-camHeightOffGround;
             LLResult result = limelight.getLatestResult();
             if (result != null && result.isValid()) {
                 List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
                 for (LLResultTypes.ColorResult cr : colorResults) {
-                    telemetry.addData("Distance", -10 / (Math.tan(Math.toRadians(cr.getTargetYDegrees()) - 10)));
+                    telemetry.addData("Distance", heightOffGround / (Math.tan(Math.toRadians(cr.getTargetYDegrees()) - a1)));
                 }
 
                 telemetry.addData("Resultsize", colorResults.size());
+            }
+            if (changer){
+                blockHeightOffGround += 0.1;
             }
             telemetry.update();
         }
