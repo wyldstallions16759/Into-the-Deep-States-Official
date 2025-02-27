d1cbc20b2d93af8ed8488016d628ba5de8e13325
wyldstallions16759 <wyldstallions16759@gmail.com>
2025-02-27 10:10:13 -0500

    Update PinpointDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointDrive.java
@@ -42,8 +42,8 @@ public class PinpointDrive extends MecanumDrive {
          */
         //These are tuned for 3110-0002-0001 Product Insight #1
         // RR localizer note: These units are inches, presets are converted from mm (which is why they are inexact)
-        public double xOffset = -3.4;
-        public double yOffset = 6.75;
+        public double xOffset = 6.8;
+        public double yOffset = 1.5;
 
         /*te
         Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
